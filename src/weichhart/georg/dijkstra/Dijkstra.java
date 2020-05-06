6615213bc5c73c16114ca06c1d44df832a8481f1
gweich <gweich@Koronis.profactor.local>
2020-05-06 21:58:35 +0200

    refactor to simplify API

--- src/weichhart/georg/dijkstra/Dijkstra.java
+++ src/weichhart/georg/dijkstra/Dijkstra.java
@@ -3,28 +3,29 @@ package weichhart.georg.dijkstra;
 import weichhart.georg.minHeap.MinHeap;
 import weichhart.georg.network.AbstractEdge;
 import weichhart.georg.network.AbstractNode;
+import weichhart.georg.network.Node;
 
 public class Dijkstra {
 
 	// global variable with the possible transport ways.
-	public static AbstractNode TransportPaths = null;
+	public static Node TransportPaths = null;
 
-	public static AbstractNode findNode(AbstractNode network, String id) {
+	public static Node findNode(Node network, String id) {
 		if (network.getId().equals(id))
 			return network;
 		for (AbstractEdge e : network.getEdgesTo()) {
-			AbstractNode nxt = findNode(e.getTo(), id);
+			Node nxt = findNode((Node) e.getTo(), id);
 			if (nxt != null)
 				return nxt;
 		}
 		return null;
 	}
 
-	public static AbstractNode searchGraph(AbstractNode network, String startnodeid, String endnodeid) {
+	public static AbstractNode searchGraph(Node network, String startnodeid, String endnodeid) {
 		return searchGraph(findNode(network, startnodeid), endnodeid);
 	}
 
-	public static AbstractNode searchGraph(AbstractNode startnode, String endnodeid) {
+	public static AbstractNode searchGraph(Node startnode, String endnodeid) {
 		initialize(startnode);
 		// all but first node have MAX distance
 		startnode.setPathValue(0);
@@ -32,27 +33,29 @@ public class Dijkstra {
 		return expand(startnode, endnodeid);
 	}
 
-	public static void initialize(AbstractNode startnode) {
+	public static void initialize(Node startnode) {
 		startnode.setPathValue(Integer.MAX_VALUE);
 		for (AbstractEdge nxt : startnode.getEdgesTo()) {
 			if (nxt.getTo().getPathValue() != Integer.MAX_VALUE)
-				initialize(nxt.getTo());
+				initialize((Node) nxt.getTo());
 		}
 	}
 
-	public static AbstractNode expand(AbstractNode gr, String endnodeid) {
+	public static AbstractNode expand(Node gr, String endnodeid) {
 		MinHeap priorityQ = new MinHeap();
 		priorityQ.addNodes(gr); // all nodes in graph
 
 		while (!priorityQ.isEmpty()) {
 			AbstractNode current = priorityQ.extractMin();
-			if (current.getId().equals(endnodeid))
-				return current;
-			for (AbstractEdge nxt : current.getEdgesTo()) {
-				if (nxt.getTo().getPathValue() > (current.getPathValue() + nxt.getWeight())) {
-					nxt.getTo().setSelectedSource(current);
-					nxt.getTo().setPathValue(current.getPathValue() + nxt.getWeight());
-					priorityQ.update(nxt.getTo());
+			if (current != AbstractNode.TERMINAL_NODE) {
+				if (current.getId().equals(endnodeid))
+					return current;
+				for (AbstractEdge nxt : current.getEdgesTo()) {
+					if (nxt.getTo().getPathValue() > (current.getPathValue() + nxt.getWeight())) {
+						((Node) nxt.getTo()).setSelectedSource(current);
+						((Node) nxt.getTo()).setPathValue(current.getPathValue() + nxt.getWeight());
+						priorityQ.update((Node) nxt.getTo());
+					}
 				}
 			}
 		}
