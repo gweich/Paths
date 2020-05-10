a9d8f4f8741a3d4562621e719485fa8d8786ac07
gweich <gweich@Koronis.profactor.local>
2020-05-10 23:02:26 +0200

    test using random generated nodes + edges

--- src/weichhart/georg/dijkstra/Main.java
+++ src/weichhart/georg/dijkstra/Main.java
@@ -1,112 +1,51 @@
 package weichhart.georg.dijkstra;
 
+import java.util.ArrayList;
+
 import weichhart.georg.network.AbstractNode;
 import weichhart.georg.network.Edge;
 import weichhart.georg.network.Node;
 
 public class Main {
 
+	// nr of nodes
+	static int nodescount = 100;
+	// 1-x edges
+	static int edgecount = 3;
+	
 	public static void main(String[] args) {
 
-		// first
-		Node n01 = new Node("0:1");
-		// last
-		Node n31 = new Node("3:1");
-
-		Node n11 = new Node("1:1");
-		Node n21 = new Node("2:1");
-		Node n10 = new Node("1:0");
-		Node n20 = new Node("2:0");
-		Node n12 = new Node("1:2");
-		Node n22 = new Node("2:2");
-
-		// start
-		Edge e = new Edge();
-		e.setWeight(1);
-		e.setTo(n11);
-		n01.addEdgeTo(e);
-		// back
-//		e = new Edge();
-//		e.setWeight(1);
-//		e.setTo(n01);
-//		n11.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n10);
-		n11.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n12);
-		n11.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n20);
-		n21.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n22);
-		n21.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n31);
-		n21.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n11);
-		n10.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n20);
-		n10.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n10);
-		n20.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n11);
-		n12.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n22);
-		n12.addEdgeTo(e);
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n12);
-		n22.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n21);
-		n20.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n21);
-		n22.addEdgeTo(e);
-
-		e = new Edge();
-		e.setWeight(1);
-		e.setTo(n21);
-		n31.addEdgeTo(e);
-
-		Dijkstra.TransportPaths = n01;
+		ArrayList<Node> nodes = new ArrayList<>(nodescount);
+		for(int i=0; i<nodescount; ++i) {
+			nodes.add(i, new Node("N"+i));
+		}
+		for(int i = 0;i<nodes.size();++i) {
+			// 1-4 Edges per Node
+			for( int e = ((int)((Math.random() * 100.0) % edgecount))+1; e>=0; --e ) {
+				Edge edge = new Edge();
+				edge.setWeight((int)(Math.random() * 30.0) + 1 );
+				edge.setTo(nodes.get((int)(Math.random() * nodescount)));
+				nodes.get((int)(Math.random() * nodescount)).addEdgeTo(edge);
+			}
+		}
 
 		System.gc();
 		Runtime rt = Runtime.getRuntime();
 		long startM = (rt.totalMemory() - rt.freeMemory());
 		long startT = System.currentTimeMillis();
 
-		AbstractNode path = Dijkstra.searchGraph(Dijkstra.TransportPaths, n01.getId(), n31.getId());
+		// start at 0
+		Dijkstra.TransportPaths = nodes.get(0);
+		AbstractNode path = Dijkstra.searchGraph(Dijkstra.TransportPaths, nodes.get(nodes.size()-1));
 
 		long endT = System.currentTimeMillis();
 		long endM = (rt.totalMemory() - rt.freeMemory());
-		System.out.println(path.toString());
-		System.out.println(path.toResult());
+		if(path!=null) {
+			System.out.println(path.toString());
+			System.out.println(path.toResult());
+		} else {
+			System.err.println("Dijkstra.searchGraph == null");
+		}
 		System.out.println("Mem / Time used: " + (endM - startM) + "B " + (endT - startT) + "ms");
 
 	}
